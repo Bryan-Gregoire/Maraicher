a1b1f8f9adc66f691f5e85a67ae43b1aba8b66e2
Ihab Tazi <55130@etu.he2b.be>
2021-10-15 11:46:47 +0200

    Defined Offer class, added authentification for users, linked users and offers

---
+++ app/Http/Controllers/OfferController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Offer;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+
+class OfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index()
+    {   //ORDER BY => choisir le critère d'ordre
+        $offers = Offer::orderBy('expirationDate')->get();
+        return view('offers.index')->with(['offers' => $offers]);
+    }
+
+    public function index_personal()
+    {
+        $user = auth()->user();
+        $offers = Offer::where('user_id', auth()->id())->orderBy('expirationDate')->get();
+        return view('offers.index')->with(['offers' => $offers]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Offer $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Offer $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Offer $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Offer $offer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Offer $offer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Offer $offer)
+    {
+        //
+    }
+}
--- database/migrations/2021_10_15_085311_create_offers_table.php
+++ database/migrations/2021_10_15_085311_create_offers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOffersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('offers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("title");
+            $table->double("price");
+            $table->integer("quantity");
+            $table->dateTime("expirationDate");
             $table->string("address");
-            $table->string("description");
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -33,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('offers');
     }
 }
