380e79762627506bce4a56fdc3371056d3aaf6b8
Ihab Tazi <55130@etu.he2b.be>
2021-10-22 08:54:27 +0200

    Fixed additional merge problems

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-<<<<<<< HEAD
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
@@ -27,13 +26,6 @@ require __DIR__ . '/auth.php';
 
 Route::resource('offers', OfferController::class);
 Route::get('/my', [OfferController::class, 'index_personal'])->name('offers.myOffers');
-=======
-Route::get('/offers', function () {
-    return view('offers/index');
-});
 
-Route::get('/offers/myOffers', function () {
-    return view('offers/myOffers' );
-});
 
->>>>>>> HTML-CSS
+
